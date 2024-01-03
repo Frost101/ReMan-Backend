ec93fdf1fbd3b6e31aa68f0ecc80c9f4a05e1689
sabahcsxgawd <sabah72224@gmail.com>
2024-01-03 21:30:33 +0600

    Adding & Testing Dummy API docs

--- controllers/retailer/retailerController.js
+++ controllers/retailer/retailerController.js
@@ -4,6 +4,51 @@ function getCartInfo(req, res) {
     });
 }
 
+function getAllNotifications(req, res) {
+
+    const notifications = [
+        { id: 1, message: 'Notification 1' },
+        { id: 2, message: 'Notification 2' },
+    ];
+
+    res.json({
+        notifications
+    });
+}
+
+function getProductListOnSale(req, res) {
+    // response should be an array of product objects
+    // each object should have the following fields:
+    // id, name, price, discount, image
+    // discount should be in percentage
+    // image should be the url of the image
+    // example response:
+    const products = [
+        {
+            id: 1,
+            name: 'Product 1',
+            price: 100,
+            discount: 10,
+            image: 'https://example.com/image1.png',
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            price: 200,
+            discount: 20,
+            image: 'https://example.com/image2.png',
+        },
+    ];
+
+    res.json({
+        products
+    });
+
+}
+
 module.exports = {
-    getCartInfo
+    getCartInfo,
+    getAllNotifications,
+    getProductListOnSale,
+
 }
--- router/retailer/retailerRouter.js
+++ router/retailer/retailerRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 //* Internal imports
-const {getCartInfo} = require('../../controllers/retailer/retailerController'); 
+const {getCartInfo,getAllNotifications, getProductListOnSale} = require('../../controllers/retailer/retailerController');
 
 //* Initialize router
 const retailerRouter = express.Router();
@@ -13,5 +13,173 @@ const retailerRouter = express.Router();
 //* /api/retailer/cartInfo
 retailerRouter.post('/cartInfo', getCartInfo);
 
+/**
+ * @swagger
+ * paths:
+ *   /api/retailer/notifications:
+ *     get:
+ *       summary: Get Retailer Notifications
+ *       description: Retrieve all notifications for a retailer client.
+ *       tags:
+ *         - Retailer
+ *       parameters:
+ *         - name: retailerId
+ *           in: query
+ *           description: Retailer's unique identifier.
+ *           required: true
+ *           schema:
+ *             type: string
+ *             example: 123456
+ *       responses:
+ *         '200':
+ *           description: Successful retrieval of notifications
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: Notification's unique identifier.
+ *                       example: abc123
+ *                     message:
+ *                       type: string
+ *                       description: Notification message.
+ *                       example: New order received.
+ *                     timestamp:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Timestamp of the notification.
+ *                       example: 2022-01-01T12:34:56Z
+ *         '400':
+ *           description: Bad Request
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Missing or invalid retailerId parameter.
+ *         '401':
+ *           description: Unauthorized
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Unauthorized. RetailerId does not match the authenticated user.
+ *         '500':
+ *           description: Internal Server Error
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Server encountered an unexpected issue.
+ *       security:
+ *         - apiKey: []
+ */
+
+retailerRouter.get('/notifications', getAllNotifications);
+
+/**
+ * @swagger
+ * paths:
+ *   /api/retailer/productsOnSale:
+ *     get:
+ *       summary: Get Products on Sale
+ *       description: Retrieve all products listed on sale by a retailer client.
+ *       tags:
+ *         - Retailer
+ *       parameters:
+ *         - name: retailerId
+ *           in: query
+ *           description: Retailer's unique identifier.
+ *           required: true
+ *           schema:
+ *             type: string
+ *             example: 123456
+ *       responses:
+ *         '200':
+ *           description: Successful retrieval of products on sale
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     productId:
+ *                       type: string
+ *                       description: Product's unique identifier.
+ *                       example: abc123
+ *                     productName:
+ *                       type: string
+ *                       description: Name of the product.
+ *                       example: Widget XYZ
+ *                     price:
+ *                       type: number
+ *                       format: float
+ *                       description: Sale price of the product.
+ *                       example: 19.99
+ *                     discountPercentage:
+ *                       type: number
+ *                       format: float
+ *                       description: Discount percentage applied to the product.
+ *                       example: 10
+ *         '400':
+ *           description: Bad Request
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Missing or invalid retailerId parameter.
+ *         '401':
+ *           description: Unauthorized
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Unauthorized. RetailerId does not match the authenticated user.
+ *         '500':
+ *           description: Internal Server Error
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Description of the error.
+ *               example:
+ *                 error: Server encountered an unexpected issue.
+ *       security:
+ *         - apiKey: []
+ */
+
+retailerRouter.get('/productsOnSale', getProductListOnSale);
 
 module.exports = retailerRouter;
