1360baaaf16fb4799c03764764e16e065a522cfe
sabahcsxgawd <sabah72224@gmail.com>
2024-01-05 03:19:30 +0600

    added postgresDB and tested prisma ORM

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 //* External imports
 const express = require('express');
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 const dotenv = require('dotenv');                  //? For environment variables
 const path = require('path');                      //? To handle file paths
 const cookieParser = require('cookie-parser');     //? To parse cookies
 const swaggerUI = require('swagger-ui-express');   //? To serve swagger docs
 const swaggerJsDoc = require('swagger-jsdoc');     //? To generate swagger docs
 
-
-
 //* Internal imports
-const { notFoundHandler, errorHandler } = require('./middlewares/common/errorHandler.js');
+const {notFoundHandler, errorHandler} = require('./middlewares/common/errorHandler.js');
 const apiRouter = require('./router/apiRouter.js');
-
 
 
 //* Load env vars
 //* Now you can use process.env.VARIABLE_NAME to access the variable
 dotenv.config();
 
-
-
 //* Initialize app
 const app = express();
 
 
-
 app.use(express.json());                             //? To Parse JSON objects in the body
-app.use(express.urlencoded({ extended: true }));     //? To Parse URL encoded data(form data) in the body. And {extended: true} parse query data
-
+app.use(express.urlencoded({extended: true}));     //? To Parse URL encoded data(form data) in the body. And {extended: true} parse query data
 
 
 //* Set view engine
 //* Now you can render ejs files but we will use React so we wont be needing it hopefully
-app.set('view engine', 'ejs');      
-
+app.set('view engine', 'ejs');
 
 
 //* Swagger Docs
@@ -58,10 +52,8 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);                      //? Gener
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));   //? Serve swagger docs
 
 
-
 //* set static public folder. Users can access files in public folder directly
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 //* Parse cookies
@@ -69,11 +61,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 
-
 //* Routes
 //* apiRouter will handle all the api requests
 app.use('/api', apiRouter);
-
 
 
 //* Error Handling Middleware
@@ -83,12 +73,61 @@ app.use(notFoundHandler);
 app.use(errorHandler);
 
 
-
 //* Start server
 app.listen(process.env.PORT, () => {
     console.log(`Server started. Listening on port ${process.env.PORT}`);
     console.log(`http://localhost:${process.env.PORT}`);
 });
+
+async function insertUser() {
+    await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+            posts: {
+                create: { title: 'Hello World' },
+            },
+            profile: {
+                create: { bio: 'I like turtles' },
+            },
+        },
+    })
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true,
+        },
+    })
+    console.dir(allUsers, { depth: null })
+}
+
+// insertUser()
+//     .then(async () => {
+//         await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//         console.error(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+//     })
+
+async function updatePost() {
+    const post = await prisma.post.updateMany({
+        data: { published: true },
+    })
+    console.log(post)
+}
+
+// updatePost()
+//     .then(async () => {
+//         await prisma.$disconnect();
+//     })
+//     .catch(async (e) => {
+//         console.error(e);
+//         await prisma.$disconnect();
+//         process.exit(1);
+//     })
 
 
 
