b121e7b271ded1b45d55b1d94cf77c44b1798253
sabahcsxgawd <sabah72224@gmail.com>
2024-01-28 18:34:10 +0600

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //* External imports
 const express = require('express');
+const cors = require('cors');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const dotenv = require('dotenv');                  //? For environment variables
@@ -28,7 +29,7 @@ dotenv.config();
 const app = express();
 
 
-
+app.use(cors());
 app.use(express.json());                             //? To Parse JSON objects in the body
 app.use(express.urlencoded({ extended: true }));     //? To Parse URL encoded data(form data) in the body. And {extended: true} parse query data
 
