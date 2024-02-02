fd3286e3a174276f69a9b26ca3c3ff36a350c94f
Piyal <piyal.abrarmahmud@gmail.com>
2024-02-02 18:44:50 +0600

    Batch List Fetched and new batch in production house added

--- controllers/batch/batchController.js
+++ controllers/batch/batchController.js
@@ -3,31 +3,60 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-function getBatchList(req, res){
-    let output = {
-        batches: [
-            {
-                BID: 123456,
-                manufacturingDate: '2021-01-01',
-                expiryDate: '2022-01-01',
-                quantity: 1000,
-            },
-            {
-                BID: 555556,
-                manufacturingDate: '2022-01-01',
-                expiryDate: '2023-01-01',
-                quantity: 1000,
-            },
-            {
-                BID: 777756,
-                manufacturingDate: '2022-01-01',
-                expiryDate: '2024-01-01',
-                quantity: 1000,
-            },
+async function getBatchList(req, res){
+
+    const iid = req.body.iid;
+    const pid = req.body.pid;
+
+    try {
+      const batches = await prisma.inventoryBatch.findMany({
+        where: {
+          iid: iid,
+          pid: pid,
+        },
+        select: {
+          bid: true,
+          ManufacturingDate: true,
+          ExpiryDate: true,
+          Quantity: true,
+          MarketStatus: true,
+          Sale: true,
+        },
+      });
+  
+      if (batches) {   
+        res.status(200).json({batches});
+      } else {
+        res.status(404).json({ error: 'No batches found' });
+      }
+    } catch (error) {
+      console.error('Error retrieving user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+    // let output = {
+    //     batches: [
+    //         {
+    //             BID: 123456,
+    //             manufacturingDate: '2021-01-01',
+    //             expiryDate: '2022-01-01',
+    //             quantity: 1000,
+    //         },
+    //         {
+    //             BID: 555556,
+    //             manufacturingDate: '2022-01-01',
+    //             expiryDate: '2023-01-01',
+    //             quantity: 1000,
+    //         },
+    //         {
+    //             BID: 777756,
+    //             manufacturingDate: '2022-01-01',
+    //             expiryDate: '2024-01-01',
+    //             quantity: 1000,
+    //         },
             
-        ],
-    };
-    res.status(200).json(output);
+    //     ],
+    // };
+    // res.status(200).json(output);
 }
 
 
@@ -38,29 +67,71 @@ function batchScreening(req, res){
 }
 
 
-function addNewBatch(req, res){
-    res.status(200).end();
+async function addNewBatch(req, res){
+    try {
+        // Extracting input parameters from the request body
+        const phid = req.body.phid;
+        const pid = req.body.pid;
+        const manufacturingDate = new Date(req.body.manufacturingDate);
+        const expiryDate = new Date(req.body.expiryDate);
+        const quantity = req.body.quantity;
+
+        // TODO: Perform any necessary validation or business logic
+
+        // TODO: Save new batch details to the database or perform other actions
+        const user = await prisma.productionHouseBatch.create({
+            data: {
+              phid: phid,
+              pid: pid,
+              ManufacturingDate: manufacturingDate,
+              ExpiryDate: expiryDate,
+              Quantity: quantity,
+            },
+          });
+
+        // Responding with success
+        res.status(201).json({
+            success: true,
+            message: 'Production House Batch created successfully',
+        });
+    } catch (error) {
+        console.error('Error adding batch:', error);
+        if ( error.code === 'P2002') {
+            // P2002 is the Prisma error code for unique constraint violation
+            console.error('Duplicate entry error:', error.meta.target);
+            res.status(409).json({
+              success: false,
+              message: 'Conflict: Production House Batch with the provided description already exists',
+            });
+        } else {
+            // Responding with server errors
+            res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+            });
+        }
+    }
 }
 
 
 async function addNewBatch1(req, res) {
     try {
         // Extracting input parameters from the request body
-        const IID = req.body.IID;
-        const PID = req.body.PID;
+        const iid = req.body.iid;
+        const pid = req.body.pid;
         const manufacturingDate = new Date(req.body.manufacturingDate);
         const expiryDate = new Date(req.body.expiryDate);
         const quantity = req.body.quantity;
         const inMarketplace = req.body.inMarketplace;
         const sale = req.body.sale;
-        
+
         // TODO: Perform any necessary validation or business logic
 
         // TODO: Save inventory details to the database or perform other actions
         const user = await prisma.inventoryBatch.create({
             data: {
-              iid: IID,
-              pid: PID,
+              iid: iid,
+              pid: pid,
               ManufacturingDate: manufacturingDate,
               ExpiryDate: expiryDate,
               Quantity: quantity,
