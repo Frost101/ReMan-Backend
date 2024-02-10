52aa843c29a52ca11dda5dacf4425980a2ec4f52
sabahcsxgawd <sabah72224@gmail.com>
2024-02-10 21:45:25 +0600

    cors test

--- app.js
+++ app.js
@@ -30,14 +30,6 @@ const app = express();
 
 
 app.use(cors());
-app.options('*', cors());
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}
-app.use(allowCrossDomain);
 
 app.use(express.json());                             //? To Parse JSON objects in the body
 app.use(express.urlencoded({ extended: true }));     //? To Parse URL encoded data(form data) in the body. And {extended: true} parse query data
