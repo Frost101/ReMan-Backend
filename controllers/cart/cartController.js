bdb98a2f8054d20e316f8bcdfb5e35cf70812d8f
Piyal <piyal.abrarmahmud@gmail.com>
2024-02-10 18:21:20 +0600

    Product added to cart

--- controllers/cart/cartController.js
+++ controllers/cart/cartController.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 function getCartInfo(req, res) {
     let output = {
         products: [{
@@ -50,12 +55,73 @@ function updateCartInfo(req, res) {
     res.json(output);
 }
 
-function addToCartInfo(req, res) {
-    let output = {
-        message: 'adding to cart successful'
-    };
+async function addToCartInfo(req, res) {
+    // let output = {
+    //     message: 'adding to cart successful'
+    // };
 
-    res.json(output);
+    // res.json(output);
+    try {
+        // Extracting input parameters from the request body
+        const {
+            sid,
+            pid,
+            Quantity,
+            Price,
+        } = req.body.product;
+
+        const deletePreviousInfo = await prisma.cart.deleteMany({
+            where: {
+              sid: sid,
+              pid: pid,
+            },
+        });
+
+        const product = await prisma.product.findUnique({
+            where: {
+              pid: pid,
+            },
+            select:{
+                MinQuantityForSale: true,
+                MinimumDeliveryCharge: true,
+                DeliveryChargeIncreaseRate: true,
+            }
+        });
+
+        const deliveryCharge = product.MinimumDeliveryCharge + (Quantity - product.MinQuantityForSale) * product.DeliveryChargeIncreaseRate;
+
+        const user = await prisma.cart.create({
+            data: {
+              sid: sid,
+              pid: pid,
+              Quantity: Quantity,
+              Price: Price,
+              DeliveryCharge: deliveryCharge,
+            },
+          });
+
+        // Responding with success
+        res.status(201).json({
+            success: true,
+            message: 'Adding to cart successful',
+        });
+    } catch (error) {
+        console.error('Error adding new cart info:', error);
+        if ( error.code === 'P2002') {
+            // P2002 is the Prisma error code for unique constraint violation
+            console.error('Duplicate entry error:', error.meta.target);
+            res.status(409).json({
+              success: false,
+              message: 'Conflict: Cart with the provided description already exists',
+            });
+        } else {
+            // Responding with server errors
+            res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+            });
+        }
+    }
 }
 
 function deleteCartInfo(req, res) {
